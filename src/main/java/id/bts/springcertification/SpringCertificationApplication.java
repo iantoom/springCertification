01d12c8a43c9c42401167b81c5eddabfcb707b03
iantoom <radityo.perwianto@gmail.com>
2019-07-02 13:20:39 +0700

    init commit

---
+++ src/main/java/id/bts/springcertification/SpringCertificationApplication.java
@@ -0,0 +1,13 @@
+package id.bts.springcertification;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringCertificationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringCertificationApplication.class, args);
+	}
+
+}
