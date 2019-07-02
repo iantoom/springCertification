69537920f1a9ebecb98f54013ba6e587dd667444
iantoom <radityo.perwianto@gmail.com>
2019-07-02 13:48:03 +0700

    add SpringBean factory and XML file

--- src/main/java/id/bts/springcertification/SpringCertificationApplication.java
+++ src/main/java/id/bts/springcertification/SpringCertificationApplication.java
@@ -1,13 +1,23 @@
 package id.bts.springcertification;
 
-import org.springframework.boot.SpringApplication;
+import org.springframework.beans.factory.support.DefaultListableBeanFactory;
+import org.springframework.beans.factory.xml.XmlBeanDefinitionReader;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.core.io.FileSystemResource;
 
 @SpringBootApplication
 public class SpringCertificationApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(SpringCertificationApplication.class, args);
+//		deprecated Library
+//		BeanFactory beanFactory = new XmlBeanFactory(resource);
+		
+		/**
+		 * Instantiate a bean factory and add a Bean Definition reader from XML files
+		 */
+		DefaultListableBeanFactory beanFactory = new DefaultListableBeanFactory();
+		XmlBeanDefinitionReader reader = new XmlBeanDefinitionReader(beanFactory);
+		reader.loadBeanDefinitions(new FileSystemResource("beans_classpath.xml"));
 	}
 
 }
