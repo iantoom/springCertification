0be0e3e6ecb3399c07499512d634a0f25cdae35b
iantoom <radityo.perwianto@gmail.com>
2019-07-02 15:34:23 +0700

    Add answer to Question 2

--- src/main/java/id/bts/springcertification/SpringCertificationApplication.java
+++ src/main/java/id/bts/springcertification/SpringCertificationApplication.java
@@ -1,13 +1,31 @@
 package id.bts.springcertification;
 
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
 
-@SpringBootApplication
+import id.bts.springcertification.service.BankServiceImpl;
+
 public class SpringCertificationApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(SpringCertificationApplication.class, args);
+//		The p namespace has to be declared
+//		Bean id is bankServiceImpl
+//		The BankServiceImpl references a NationalBank bean
+//		NationalBank is a scalar value
+		
+		/**
+		 * load class_path XML which declare xmlns:p 
+		 */
+		@SuppressWarnings("resource")
+		ApplicationContext context = new ClassPathXmlApplicationContext("class_path.xml");
+		
+		/**
+		 * Since the BankServiceImpl Bean is anonymous, the id is auto generated
+		 * The format is "qualified.class.name#number"
+		 */
+		BankServiceImpl bankService = (BankServiceImpl) context.getBean("id.bts.springcertification.service.BankServiceImpl#0");
+		
+		System.out.println(bankService.getBankName());
 	}
 
 }
---
+++ src/main/java/id/bts/springcertification/service/BankServiceImpl.java
@@ -0,0 +1,15 @@
+package id.bts.springcertification.service;
+
+public class BankServiceImpl {
+
+	private String bankName;
+
+	public String getBankName() {
+		return bankName;
+	}
+
+	public void setBankName(String bankName) {
+		this.bankName = bankName;
+	}
+	
+}
